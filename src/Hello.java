3757009c55396d3c8d83674084e4e8a9bdca9cdd
Administrator <zhaowanyue1118@163.com>
2018-06-01 23:08:46 +0800

    test

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args)
     {
-        System.out.print("hello world!");
+        System.out.print("hello world!dd");
+        System.out.print("hello world!dd");
     }
 }
